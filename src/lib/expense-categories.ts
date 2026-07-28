665429200ddb88745e6c9cc86bae3337ab6420a6
Jahez Abraham Johny <jahez.john@gmail.com>
2026-07-28 09:17:45 +0400

    expense categories

---
+++ src/lib/expense-categories.ts
@@ -0,0 +1,12 @@
+import { ExpenseCategory } from "@/types";
+
+export const EXPENSE_CATEGORIES: ExpenseCategory[] = [
+  "Food & Dining",
+  "Groceries",
+  "Snacks",
+  "Entertainment",
+  "Family",
+  "Gifts",
+  "Health",
+  "Shopping",
+];
